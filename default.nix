44f60ed736bf28b3e12572c9e0da6f78fa8191af
Graham Christensen <graham@grahamc.com>
2020-03-26 16:20:16 -0400

    mkPoetryApplication: make it work? Not sure..

--- default.nix
+++ default.nix
@@ -1,16 +1,19 @@
 { pkgs ? import <nixpkgs> {} }:
-
 let
-
   overrides = import ./overrides.nix { inherit pkgs; };
 
 in pkgs.poetry2nix.mkPoetryApplication {
   projectDir = ./.;
+  src = ./.;
+  pyproject = ./pyproject.toml;
+  poetrylock = ./poetry.lock;
+
 
   propagatedBuildInputs = [
     pkgs.openssh
   ];
 
-  overrides = pkgs.poetry2nix.overrides.withDefaults(overrides);
-
+  overrides = [
+    overrides
+  ];
 }
